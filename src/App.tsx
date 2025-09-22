0219cafd680bd924405c5bf912624168db890904
Pxxp116 <p.equix25@gmail.com>
2025-09-22 12:18:16 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,27 @@
-import { Toaster } from "@/components/ui/toaster";
-import { Toaster as Sonner } from "@/components/ui/sonner";
-import { TooltipProvider } from "@/components/ui/tooltip";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Index from "./pages/Index";
-import NotFound from "./pages/NotFound";
+import Navbar from "./components/Navbar";
+import HeroSection from "./components/HeroSection";
+import SolutionsSection from "./components/SolutionsSection";
+import AutomationSection from "./components/AutomationSection";
+import SplitQRSection from "./components/SplitQRSection";
+import BenefitsSection from "./components/BenefitsSection";
+import PlansSection from "./components/PlansSection";
+import FAQSection from "./components/FAQSection";
+import Footer from "./components/Footer";
 
-const queryClient = new QueryClient();
-
-const App = () => (
-  <QueryClientProvider client={queryClient}>
-    <TooltipProvider>
-      <Toaster />
-      <Sonner />
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Index />} />
-          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </BrowserRouter>
-    </TooltipProvider>
-  </QueryClientProvider>
-);
+function App() {
+  return (
+    <div className="min-h-screen bg-background text-foreground">
+      <Navbar />
+      <HeroSection />
+      <SolutionsSection />
+      <AutomationSection />
+      <SplitQRSection />
+      <BenefitsSection />
+      <PlansSection />
+      <FAQSection />
+      <Footer />
+    </div>
+  );
+}
 
 export default App;
