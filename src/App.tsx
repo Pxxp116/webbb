e42a1cf7ed598ffaaca2076b180baab6abebeceb
Nicolás Díaz <p.equix25@gmail.com>
2025-10-07 20:13:34 +0200

    Fix: corregida ruta del componente Navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router } from "react-router-dom";
-import Navbar from "./components/layout/Navigation";
+import Navbar from "./components/layout/Navbar";
 import Footer from "./components/layout/Footer";
 import Hero from "./components/sections/Hero";
 import Productos from "./components/sections/Productos";
